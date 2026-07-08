079d9e5a10430459ecfbcf8261f16ee3d5b05163
yoonsung157 <yoonsung157@gmail.com>
2026-07-08 13:28:48 +0900

    day03 4h

---
+++ day03/exam1.js
@@ -0,0 +1,157 @@
+console.log('JS 실행')
+// 한줄주석
+/* 여러줄주석 */
+
+// [1] 숫자타입
+// 1. 정수 
+console.log( 10 )
+console.log( typeof 10 ) // typeof 자료, 해당 자료의 타입명 확인
+// 2. 실수 : 부동소수점( 컴퓨터가 실수를 처리하는 방법)
+console.log( 3.14 )
+console.log( typeof 3.14 )
+
+// [2] 문자열타입
+// 1. ' , " , ` , 감싼 자료
+console.log('안녕')
+console.log("안녕")
+console.log(`안녕`)
+// 2. 문자열 연결
+console.log('안녕', '하세요')
+console.log('안녕' + '하세요')
+console.log(`안녕 ${ 3 }`) // 문자열 템플릿 ? 문자열내 코드 포함
+console.log('안녕' + 3) // 문자열 + 숫자 => 문자열
+let a = 10
+console.log('안녕' + a) // 문자열 + 변수(숫자) => 문자열
+console.log(`안녕 ${ a }`) //문자열 템플릿은 `백틱으로 감싼 자료 안에 ${} 이용하여 코드 포함
+// 3. 이스케이프(제어) 문자 , \ 백슬래시
+console.log('\\안녕')       // '\' 출력
+console.log('\n안녕')       // '\n' 줄바꿈
+console.log('\'안녕')       // 문자열 감싼 기호화 그 자체 기호를 구분
+console.log('\t안녕')       // '\t' 들여쓰기
+
+// [3] 불리언(논리) 타입
+console.log(true)       // 참 = 1
+console.log(false)      // 거짓 = 0
+console.log(10 > 3)     // 비교연산자의 반환값
+
+// [4] undefined, null  ,   컴퓨터는 0 vs ' ' vs null(참조가 없다) vs undefined(정의가 없다) 모두 다르게 취급
+let 과일상자            // 변수선언 : let 변수명 
+console.log(과일상자)   // undefined : 변수가 선언 되었지만 초기값이 없다.
+let 채소상자 = null     // 변수선언 ok 초기화 ok, 
+console.log(null)       // null
+
+// [5] 배열 , 순서대로 나열
+// [ ] 대괄호 안에 , 쉼표를 이용하여 여러 개의 자료들을 *하나의 자료*로 표현 , 핸드폰과 지갑을 가방에 넣었다.
+// 인덱스(색인) : [ ] 해당 배열 내 저장된 자료들의 순서 번호 , 0번부터 시작 
+let 배열변수 = [ '봄' , '여름' , '가을' , '겨울', [ '1학기', '2학기']]
+console.log(배열변수)           //배열 전체 호출
+console.log(배열변수[0])        // 특정 인덱스의 자료(요소) 호출
+console.log(배열변수[4])        // 4 인덱스의 자료 호출
+console.log(배열변수[4][1])     // 2학기
+// vs
+let 요소값1 = 배열변수[4]       // (1) 4번 인덱스 요소 꺼내고 변수에 저장
+let 요소값2 = 요소값1[1]        // (2) 1번 인덱스 요소 꺼내고 변수에 저장
+console.log(요소값2)
+
+// 2. 배열 수정 , 변수명 = 새로운배열           * 변수(자료저장소) vs 배열(자료) *
+배열변수 = [ '유재석', '강호동' ]       // 변수는 하나의 자료를 저장하기 때문에 새로운 대입이 들어오면 기존 대입은 사라진다.
+console.log(배열변수)
+배열변수[0] = '유재석2'                 // 0번 자리에 유재석을 유재석2로 변경한다.
+console.log(배열변수)
+// let a = [ 10 ] , let b = [ 10 ]      // 가방은 다르지만 안의 내용은 같다.
+
+// 3. 배열 요소 추가
+배열변수.push('서장훈')                 // 마지막 자료 뒤에 새로운 자료 추가
+console.log(배열변수)
+
+// 4. 배열 요소 삭제 , splice(삭제할인덱스, 개수)
+배열변수.splice(0, 1)                   // 0번 인덱스(자리)에 자료(요소) 1개 삭제한다.
+console.log(배열변수)
+
+// 5. 배열 요소 찾기 , .indexOf( 찾을값 ) , 만약에 찾을값이 존재하면 인덱스 반환 없으면 -1
+let 찾은인덱스 = 배열변수.indexOf('강호동')
+console.log( 찾은인덱스)                // 0번 인덱스에 찾을 '강호동' 존재한다.
+
+// .includes( 찾을값 ) , 만약에 찾을값이 존재하면 true 없으면 false 반환
+let 존재여부 = 배열변수.includes( '강호동' )
+console.log( 존재여부 )
+
+// [6] 타입 변환 ,  3 vs "3" 숫자3과 문자3은 다르다 , 실수는 JS만으로 개발[x] 외부언어/자료 혼합[O]
+// 서로 다른 언어/자료들간의 통산 HTTP 통신한다. JSON/XML/CSV 타입 (문자열)
+// 1. 자동 타입 변환
+console.log( 3 + 3.14 )                 // 정수 + 실수 => 실수
+console.log('유재석' + 40)              // 문자 + 숫자 => 문자
+console.log(3 + true)                   // 정수 + 논리 => 숫자(숫자) 4
+// 2. 직접 타입 변환
+console.log(Number( "10" ) )            // 문자열 10 -> 숫자형 10으로 변환
+console.log(Number("10A"))              // NaN( NOT A NUMBER ) , 10A -> 숫자형으로 변환 불가능
+console.log(parseInt( "10" ))           // 문자열 10 -> 숫자(정수) 10으로 변환
+console.log(parseFloat ("3.14"))        // 문자열 3.14 -> 숫자(실수) 3.14로 변환
+console.log(String( 10 ))               // 숫자형 10을 -> 문자열 10으로 변환
+console.log( 10 + "" )
+// 추후에 JAVA/PYTHON/API(외부자료) 들을 통신한경우
+// JSON.parse( 통신결과물 )
+
+// [7] 연산자
+// 1. 산술연산자 : +더하기 -빼기 *곱하기 /나누기 %나머지
+// 2. 연결연산자 : +연결연산자
+// 3. 비교연산자 : >초과 <미만 >=이상 <=이하 ==(값)같다 !=(값)같지않다. ===(값/타입) !==(값/타입)같지않다.
+// 4. 논리연산자 : &&이면서 ||이거나 !부정 
+// 5. 대입연산자 : = 오른쪽항을 왼쪽항에 대입
+// + 복합대입연산자 : += -= *= /= %= 오른쪽항의 값을 왼쪽항에 연산후 대입
+// 6. 증감연산자 : ++1증가 --1감소 
+// * ++변수명 : 선위증가 , 변수명++ : 후위증가 , --변수명 : 선위감소, 변수명-- : 후위감소
+// 7. 삼항연산자 : 조건 ? 참 : 거짓     ,   조건A ? 참A : 조건B ? 참B : 조건C ? 참C : 거짓
+// * 점수 >= 90 ? '합격' : '불합격'
+
+// [지문1] prompt 함수로 국어 , 영어 , 수학 점수를 각 입력받아서 (각 변수에 저장하고) 총점 과 평균을 계산하여 console탭에 출력하시오.
+// let 국어점수 = Number(prompt("국어점수"))
+// let 영어점수 = Number(prompt("영어점수"))
+// let 수학점수 = Number(prompt("수학점수"))
+// let 총점 = 국어점수 + 영어점수 + 수학점수
+// let 평균 = 총점 / 3 
+
+// console.log(`총점 : ${총점} \n 평균 : ${평균}`)
+// [지문2] prompt 함수로 반지름를 입력받아서 원넓이[반지름*반지름*3.14] 계산하여 console탭에 출력하시오.
+// let r = prompt("반지름")
+// console.log(`넓이 : ${r * r * 3.14}`)
+
+// [지문3] prompt 함수로 두 실수를 입력받아서 앞 실수의 값이 뒤실수의 값의 비율% 계산하여console탭에 출력하시오.
+// let d = prompt("첫번째 실수")
+// let f = prompt("두번째 실수")
+
+// console.log(`비율 : ${d / f * 100}%`)
+
+// [지문4] prompt 함수로 정수를 입력받아 입력받은 값이 홀수이면 true / 짝수이면 false 로console탭에 출력하시오.
+// let d = prompt("정수 입력")
+// console.log(`${d % 2 != 0 ? true : false}`)
+
+// [지문5] prompt 함수로 정수를 입력받아 입력받은 값이 7의 배수이면 true / 아니면 false 로console탭에 출력하시오.
+// let d = prompt("정수입력")
+// console.log(`${ d % 7 == 0 ? true : false}`)
+
+// [지문6] prompt 함수로 아이디 와 비밀번호를 입력받아서 (입력받은)아이디가 'admin' 이고 (입력받은)비밀번호가 1234 와 일치하면 true / 아니면 false 출력하시오.
+// let id = prompt("아이디")
+// let password = prompt("비밀번호")
+
+// console.log(`${id == 'admin' && password == '1234' ? true : false}`)
+
+// [지문7] prompt 함수로 정수를 입력받아 입력받은 값이 홀수 이거나 7배수 이면 true / 아니면false 로 console탭에 출력하시오.
+// let d = prompt("정수입력")
+// console.log(`${d % 2 != 0 || d % 7 == 0 ? true : false}`)
+
+// [지문8] 1차점수 와 2차점수 prompt함수로 각 입력받아서 총점이 150점이상이면 '합격' 아니면'불합격' HTML의 <h3> 에 출력하시오.
+// let score1 = Number(prompt("1차점수"))
+// let score2 = Number(prompt("2차점수"))
+// let sum = score1 + score2
+// let result = sum >= 150 ? '합격' : '불합격'
+
+// document.write(`<h3>${result}</h3>`)
+
+
+// [지문9] 두 사람의 이름을 prompt함수로 각 입력받아서 만일 이름이 '유재석' 이면 뒤에 (방장) 이라고 이름 뒤에 붙이고 아니면 생략한다.  HTML의 <ol> 에 결과를 출력하시오.
+let name1 = prompt("이름")
+let name2 = prompt('이름')
+let result1 = name1 == '유재석' ? '${name1}(방장)' : name1
+console.log(result1)
+
