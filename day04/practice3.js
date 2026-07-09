c1d7a4f74c98f33b4ecb07d8192b6e1031cc7799
yoonsung157 <yoonsung157@gmail.com>
2026-07-09 18:49:25 +0900

    day04 8h

--- day04/practice3.js
+++ day04/practice3.js
@@ -66,3 +66,94 @@
 //     if (b > c) console.log(b)
 //     else console.log(c)
 // }
+
+// 문제 6: 윤년 판별기
+// 사용자로부터 연도(year)를 입력받아 해당 연도가 윤년인지 평년인지 판별하는 프로그램을 작성하시오.
+// 윤년 조건:
+// 연도가 4의 배수이면서, 100의 배수는 아닐 때
+// 또는 연도가 400의 배수일 때
+// 출력 예시: '2024년은 윤년입니다.' 또는 '2023년은 평년입니다.'
+
+// let year = prompt('태어난 연도')
+
+// if( year % 4 == 0 && year % 100 != 0) console.log(`${year}년은 윤년입니다.`)
+// else if ( year % 400 == 0 ) console.log(`${year}년은 윤년입니다.`)
+// else console.log(`${year}년은 평년입니다.`)
+
+// 문제 7: 세 수 오름차순 정렬
+// 서로 다른 세 개의 정수를 입력받아, 오름차순(작은 수부터 큰 수 순서)으로 정렬하여 출력하는 프로그램을 작성하시오.
+// 예시: 17, 4, 8 입력 시 4, 8, 17 출력
+
+// let a = Number(prompt('정수 입력'))
+// let b = Number(prompt('정수 입력'))
+// let c = Number(prompt('정수 입력'))
+
+// if ( a > b ) {
+//     if ( a > c ) {
+//         b > c ? console.log(`${a},${b},${c}`) : console.log(`${a},${c},${b}`)
+//     }
+//     else console.log(`${c}, ${a}, ${b}`) //c>a>b
+// }
+// else {
+//     if ( b > c) {
+//         a > c ? console.log(`${b}, ${a}, ${c}`) : console.log(`${b},${c},${a}`)
+//     }
+//     else console.log(`${c}, ${b}, ${a}`)
+// }
+
+// 문제 8: 가위바위보 게임
+// 두 명의 플레이어가 참여하는 가위바위보 게임을 만드시오.
+// 입력: 플레이어 1과 플레이어 2는 각각 0(가위), 1(바위), 2(보) 중 하나의 숫자를 입력합니다.
+// 규칙:
+// 가위(0)는 보(2)를 이깁니다.
+// 바위(1)는 가위(0)를 이깁니다.
+// 보(2)는 바위(1)를 이깁니다.
+// 출력:
+// 플레이어 1이 이기면 '플레이어1 승리'를 출력합니다.
+// 플레이어 2가 이기면 '플레이어2 승리'를 출력합니다.
+// 두 플레이어가 같은 것을 내면 '무승부'를 출력합니다.
+
+// let player1 = prompt('0(가위), 1(바위), 2(보)')
+// let player2 = prompt('0(가위), 1(바위), 2(보)')
+
+// if(player1 == player2) console.log('무승부')
+// else {
+//     if( player1 == 0 && player2 == 1) console.log('player2승리')
+//     else if ( player1 == 0 && player2 == 2) console.log('player1승리')
+//     else if ( player1 == 1 && player2 == 0) console.log('player1승리')
+//     else if ( player1 == 1 && player2 == 2) console.log('player2승리')
+//     else if ( player1 == 2 && player2 == 0) console.log('player2승리')
+//     else console.log('player1승리')
+// }
+
+
+// 문제 9: 주차 차량 위치 검색
+// 주차장 차량 정보가 다음과 같이 두 개의 배열로 관리되고 있습니다. 두 배열에서 같은 인덱스는 동일한 차량의 정보를 의미합니다.
+// 차량 번호 목록: let carArray = [ '250어7142', '142가7415', '888호8888' ];
+// 주차 위치 목록: let locationArray = [ 'A1', 'B3', 'C2' ];
+// 사용자로부터 차량 번호를 입력받아, carArray에서 해당 차량을 찾은 뒤 locationArray에 있는 그 차량의 주차 위치를 찾아 출력하는 프로그
+// 램을 작성하시오. 만약 해당하는 차량 번호가 없다면 '차량이 존재하지 않습니다.'를 출력하시오.
+
+// let carArray = [ '250어7142', '142가7415', '888호8888' ]
+// let locationArray = [ 'A1', 'B3', 'C2' ]
+
+// let carnumber = prompt('차량번호를 입력하세요')
+
+// if (carArray.includes(carnumber)) console.log(locationArray[carArray.indexOf(carnumber)])
+// else console.log('차량이 존재하지 않습니다.')
+
+// 문제 10: 수강 신청 목록에서 과목 제외하기
+// 현재 수강 신청한 과목 목록이 배열로 주어져 있습니다.
+// let courseList = ['수학', '영어', '과학', '국어'];
+// 사용자로부터 제외하고 싶은 과목명을 입력받아, courseList에 해당 과목이 존재하면 목록에서 삭제한 뒤 변경된 배열 전체를 출력하세요. 만약
+// 존재하지 않는 과목이면 '해당 과목은 신청 목록에 없습니다.'를 출력하는 프로그램을 작성하시오.
+
+let courseList = ['수학', '영어', '과학', '국어']
+
+let sub = prompt('과목 입력')
+
+if( courseList.includes(sub) ) {
+    courseList.splice(courseList.indexOf(sub),1)
+    console.log(courseList)
+}
+else console.log('해당 과목은 신청 목록에 없습니다.') 
