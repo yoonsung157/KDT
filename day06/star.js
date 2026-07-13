4049b949421456f8184a19a6b8cc821d7809e48b
yoonsung157 <yoonsung157@gmail.com>
2026-07-13 17:02:24 +0900

    day06 7h

---
+++ day06/star.js
@@ -0,0 +1,17 @@
+let html = ''
+
+
+for(let i = 0; i < 10; i++ ){
+    for(let k = 0; k <= i; k++){
+        html += ' ★ '
+    } 
+    html += '</br>'
+}
+for(let i = 0; i < 10; i++){
+    for(let k = 0; 10 - k > i; k++){
+        html += ' ★ '
+    }
+    html += '</br>'
+}
+
+document.querySelector("p").innerHTML = html
