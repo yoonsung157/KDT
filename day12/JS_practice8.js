6224edd88a610852fb7fd2134a0c74f631d77049
yoonsung157 <yoonsung157@gmail.com>
2026-07-22 18:06:49 +0900

    day12 8h

--- day12/JS_practice8.js
+++ day12/JS_practice8.js
@@ -56,3 +56,29 @@ function productUpdate( pcode ) {
     }
 }
 // [4] 등록 함수
+let finalPcode = 2 // 현재 마지막으로 사용한 제품코드 
+function productAdd( ){
+    // 1. 입력받은 값 가져오기 
+    let category = document.querySelector('.category').value
+    let name = document.querySelector('.name').value
+    let price = document.querySelector('.price').value
+    let image = document.querySelector('.image').files[0] // + 첨부파일은 .files[0] 속성에서 첨부파일의 첫번째 자료 가져오기 
+    if( category == 'disabled' ) {
+        alert('카테고리 선택해주세요!')
+        return;
+    }
+    // 2. 입력받은 값들을 객체화
+        // pcode : 제품식별코드로 사용자가 지정하지 않고 *자동번호 부여* , 마지막사용된 제품코드에 + 1
+        // pdate : 현재 시스템 날짜/시간 함수 , new Date()
+            // 현재연도 : new Date().getFullYear()  ,  현재월(0:1월~11:12월) : new Date().getMonth() , 현재일 : new Date().getDate()
+        //console.log( new Date().getFullYear() , new Date().getMonth() , new Date().getDate()  ) 
+    let pdate = `${ new Date().getFullYear() }-${ new Date().getMonth()+1 }-${ new Date().getDate() }`
+    let object = { ccode : category , pname : name , pprice : price , 
+                    pimg : image == undefined ? 'https://placehold.co/100' : URL.createObjectURL( image ) , 
+                    pcode : finalPcode+1 , pdate : pdate }
+    // 3. 배열 저장  , **마지막 제품코드 1 증가** 
+    productList.push( object ); finalPcode += 1
+    // 4. 성공  , 화면 최신화 
+    alert('등록성공'); 
+    productPrint();
+} // f end 
--- day12/JS_practice9.js
+++ day12/JS_practice9.js
@@ -66,3 +66,154 @@ function partUpdate( pcode ) {
 }
 
 showPart( )
+
+//휴가
+showVaction()
+
+//휴가신청 출력
+function showVaction(){
+    let html=''
+    let name=''
+    for(let i=0; i<=vacationList.length-1; i++){
+        for(let j=0; j<=memberList.length-1; j++){
+            if(vacationList[i].mcode==memberList[j].mcode){
+                name=memberList[j].mname
+                break
+            }
+        }
+        html+=`<div class="vacation-card">
+                <div class="user-name">${name}</div>
+                <div class="date">${vacationList[i].vstart}~${vacationList[i].vend}</div>
+                <div class="reason">${vacationList[i].vreason}</div>
+                <button onclick="vacationDelete(${vacationList[i].vcode})" type="button" class="btn-cancel">신청취소</button>
+            </div>`
+    }
+    document.querySelector('.vacation_sub').innerHTML=html
+}
+
+//휴가신청
+function vacationAdd(){
+    //휴가사원 이름 가져오기
+    let member = document.querySelector('.member_select').value
+    let mcode =null
+    //휴가사원의 사원코드 찾아오기
+    for(let i=0; i<=memberList.length-1; i++){
+        if(member == memberList[i].mname){
+            mcode = memberList[i].mcode
+            break
+        }
+    }
+    //휴가시작-끝 날짜 가져오기
+    let vstart = document.querySelector('.vacation_start').value
+    let vend = document.querySelector('.vacation_end').value
+    //휴가사유 가져오기
+    let vreason = document.querySelector('.vacation_reason').value
+    //vationList 에 추가
+    let vacationSub={mcode,vstart,vend,vreason}
+    vacationList.push(vacationSub)
+    //출력
+    showVaction()
+}
+
+
+//휴가신청 취소
+function vacationDelete(vcode){
+    //휴가사원의 사원코드 찾아오기
+    for(let i=0; i<=vacationList.length-1; i++){
+        if(vcode == vacationList[i].vcode){
+            vacationList.splice( i , 1 );
+            alert('신청취소됨.');
+        }
+    }
+    //출력
+    showVaction()
+}
+
+function showMember() {
+    
+    let html = ''
+    // pcode에 맞는 pname 찾기
+    // pcode는 partList에 있음
+
+    for (let index = 0; index < memberList.length; index++){
+
+        // pcode에 맞는 pname을 찾고, pname에 선언
+        let temp = memberList[index].pcode
+    
+        result = partList.find(item => item.pcode == temp)
+
+
+        html += `
+            <tr>
+              <td><img src=${memberList[index].mimg} alt="프로필" class="profile-img"></td>
+              <td>${memberList[index].mname}</td>
+              <td>${result.pname}</td>
+              <td>${memberList[index].mposition}</td>
+              <td class="action-links align-right">
+                <a href="#" onclick="memberUpdate(${memberList[index].mcode})" class="link-edit">수정</a>
+                <a href="#" onclick="memberDelete(${memberList[index].mcode})" class="link-delete">삭제</a>
+              </td>
+            </tr>
+        `
+    }
+    document.querySelector('.card.sub-section tbody').innerHTML = html
+} showMember(); // 함수선언 후 사원목록 출력
+
+function memberUpdate(mcode){
+
+    for (index=0; index<memberList.length; index++){
+        if(mcode == memberList[index].mcode){
+            let newMebmerName = prompt("새로운 이름을 입력하시오")
+            let newMemberPart = prompt("새로운 부서를 입력하시오")
+
+            // 없는 부서일 경우 실패
+            for (j=0; j<partList.length; j++){
+                if (partList.find(item => item.pname === newMemberPart)){
+                    newMemberPart = partList[j].pcode
+                    break;
+                }
+                else{
+                    alert("없는 부서입니다")
+                    return
+                }
+            }
+
+            let newMemberPosition = prompt("새로운 직급을 입력하시오")
+
+            memberList[index].mname = newMebmerName
+            memberList[index].pcode = newMemberPart
+            memberList[index].mposition = newMemberPosition
+            break;
+        }
+    }
+
+    showMember()
+    return
+}
+function memberDelete(mcode){
+    for (index=0; index <memberList.length; index++){
+        if(mcode == memberList[index].mcode){
+            memberList.splice(index, 1)
+            alert("삭제성공")
+            break;
+        }
+    }
+    showMember()
+    return
+}
+
+let memberCode = memberList[memberList.length-1].mcode
+function memberAdd(){
+    
+    memberCode += 1
+    let memberName = document.querySelector('.memberName').value
+    let memberPosition = document.querySelector('.memberPosition').value
+    let memberPart = document.querySelector('.memberPart').value
+    let memberImage = document.querySelector('.memberImg').files[0]
+
+    
+    let object = {mcode: memberCode, mname: memberName, mposition:memberPosition, mimg: memberImage == undefined ? 'https://placehold.co/100' : URL.createObjectURL(image),pcode:0}
+    console.log(memberCode)	
+    memberList.push(object)
+    showMember();
+}
