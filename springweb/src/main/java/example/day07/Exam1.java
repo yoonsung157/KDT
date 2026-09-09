ddf15299172d45ea9ab7d6de4aa68918c23c754c
yoonsung157 <yoonsung157@gmail.com>
2026-09-09 10:17:33 +0900

    수업

---
+++ springweb/src/main/java/example/day07/Exam1.java
@@ -0,0 +1,38 @@
+package example.day07;
+public class Exam1 {
+    public static void main(String[] args ) {
+        // 서로 다른 클래스에 메소드 호출하는 방법
+        // 메소드란? 상호작용
+        // 1. 인스턴스 생성하여 메소드 호출
+        TestService testService = new TestService();
+        int result = testService.plus(3, 5);
+
+        // 2. 싱글톤
+        TestService testService2 = TestService.getInstance();
+        int result2 = testService2.plus(1,2);
+
+        // 3. 메소드가 static 이면
+        int result3 = TestService.plus2(10, 5);
+
+        // 4. 스프링방식
+        // @Service (자동 인스턴스 생성) @Autowired(인스턴스 호출)
+    }
+}
+
+class TestService {
+    private TestService(){}
+    private static final TestService instance = new TestService();
+    public static TestService getInstance(){ return instance; }
+
+    int plus(int x, int y) {
+        return x + y;
+    }
+    static int plus2(int x, int y) { return x + y; }
+}
+
+class Student {
+    String name;
+    void 밥먹기() {
+        System.out.println( this.name + "밥 먹는다. ");
+    }
+}
--- springweb/src/main/java/example/practice5_1/service/CommentService.java
+++
@@ -1,5 +0,0 @@
-package example.practice5_1.service;
-
-public class CommentService {
-    
-}
